d2cb2488c56b1fa83c509d340d064f4c5230f7f7
Kayvan Vakili <kvakili813@gmail.com>
2018-01-25 03:26:27 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,6 +2,6 @@ def oxford_comma(array)
   if array.length == 1
     puts array.join
   end
-  array.join(" and ")
-  array[0..-2].join(', ') + " and " + array[-1] if array.length > 1
+  #array.join(" and ")
+  #array[0..-2].join(', ') + " and " + array[-1] if array.length > 1
 end
