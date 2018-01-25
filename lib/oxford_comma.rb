bcb9aed4c97eaf33a58970e0834a2819a7d5a3a6
Kayvan Vakili <kvakili813@gmail.com>
2018-01-25 03:29:29 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,7 +1,9 @@
 def oxford_comma(array)
   if array.length == 1
     array.join
-  end
-  #array.join(" and ")
+  
+  elsif array.length == 2
+    array.join(" and ")
   #array[0..-2].join(', ') + " and " + array[-1] if array.length > 1
+end
 end
